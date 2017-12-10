0495c50ed5c352fd668a63d3832aca6ef76b5374
Dylan Neve <dylanneve1@users.noreply.github.com>
2017-12-10 23:02:38 +0000

    Add names.py

---
+++ names.py
@@ -0,0 +1,14 @@
+''' Name program '''
+
+# Add names
+def names(x, y):
+    return x + y
+
+print("Hello my name is Dylan, what is yours?\n\n")
+
+# User input
+firstName = input("Enter first name: ")
+secondName = input("Enter second name: ")
+
+# Print input
+print(name(firstName, secondName)
