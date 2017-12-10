bafacfd6580642d1d351e256a0d8f151aba173ae
Dylan Neve <dylanneve1@users.noreply.github.com>
2017-12-10 23:20:11 +0000

    Add ai test

---
+++ ai.py
@@ -0,0 +1,12 @@
+''' Test AI '''
+
+print("H...h...hello?... Where am I?")
+
+place = input("")
+
+print("Oh... so I'm " + place)
+print("What's your name?")
+
+name = input("")
+
+print("Hello " + name)
