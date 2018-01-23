d3810b5a353433436ee41b73b6c2ef581366033b
Dylan Neve <dylanneve1@users.noreply.github.com>
2018-01-23 20:28:33 +0000

    Start add learner

---
+++ leaner.py
@@ -0,0 +1,30 @@
+''' Leaner test 1.0 '''
+
+# Add info
+
+def names(x, y):
+    return x + " " + y
+
+def where(x, y):
+    return x + " which is in " + y
+
+# Start
+
+print("Hi. What's your name?")
+
+print("\n\n")
+
+firstName = input("My name is ")
+
+print("\n\n")
+
+print("Oh, hi " + firstName + ". What country do you live in?")
+
+print("\n\n")
+
+country = input("I live in ")
+
+print("\n\n")
+
+print("So you're " + firstName + " from " + country)
+
