4b39e0ff9de84ca6e678b9fb546257005d0356f8
Dylan Neve <dylanneve1@users.noreply.github.com>
2018-01-23 20:42:18 +0000

    Update guessing game

--- number_guess.py
+++ number_guess.py
@@ -8,10 +8,19 @@ num = random.randint(1,100)
 
 print("I'm thinking of a number between 1 and 100. Guess it!")
 
-while x == x:
+while x == 0:
+    
     theGuess = int(input("Your guess: "))
+    
     if theGuess == num:
         print("You got it!")
         x = 1
+        
+    elif theGuess > num:
+        print("Try again, it's a smaller number")
+
+    elif theGuess < num:
+        print("Try again, it's a bigger number")
+
     else:
-        print("Try again")
+        print("That is not a valid number. Please try again")
